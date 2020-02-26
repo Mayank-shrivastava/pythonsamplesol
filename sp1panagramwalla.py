8e512411b4ff1aa6de3743110347d66e3017a1cb
Mayank-shrivastava <59906606+Mayank-shrivastava@users.noreply.github.com>
2020-02-26 11:59:33 +0530

    Add files via upload
    fbgdjgh

---
+++ sp1panagramwalla.py
@@ -0,0 +1,9 @@
+alphabet="abcdefghijklmnopqrstuvwxyz"
+s=input()
+for char in alphabet: 
+        if char not in s.lower(): 
+            print("no panagram")
+        
+        print("yes panagram")
+        break    
+     
