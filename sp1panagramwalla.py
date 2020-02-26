7b8d92beaa176079a959079942e61eef8045c801
Mayank-shrivastava <59906606+Mayank-shrivastava@users.noreply.github.com>
2020-02-26 21:24:59 +0530

    Delete sp1panagramwalla.py

--- sp1panagramwalla.py
+++
@@ -1,9 +0,0 @@
-alphabet="abcdefghijklmnopqrstuvwxyz"
-s=input()
-for char in alphabet: 
-        if char not in s.lower(): 
-            print("no panagram")
-        
-        print("yes panagram")
-        break    
-     
